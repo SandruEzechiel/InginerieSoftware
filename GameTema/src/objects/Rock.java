f5792a586152e295c1c27a1fb544be8d29919a18
Ezy <146414741+SandruEzechiel@users.noreply.github.com>
2025-11-13 22:58:20 +0200

    fix

--- GameTema/src/objects/Rock.java
+++ GameTema/src/objects/Rock.java
@@ -1,14 +1,13 @@
 package objects;
-
 import game.GameObject;
 
 public class Rock extends GameObject {
     public Rock(int x, int y) {
         super("Rock", x, y);
     }
-
     @Override
     public void update() {
         System.out.println(name + " does nothing.");
     }
 }
+
