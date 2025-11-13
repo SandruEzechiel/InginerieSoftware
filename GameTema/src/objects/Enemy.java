4da09a67be4a190c869b1a5d27b01873fcae3506
Ezy <146414741+SandruEzechiel@users.noreply.github.com>
2025-11-13 22:57:49 +0200

    fix

--- GameTema/src/objects/Enemy.java
+++ GameTema/src/objects/Enemy.java
@@ -1,14 +1,13 @@
 package objects;
-
 import game.GameObject;
 
 public class Enemy extends GameObject {
     public Enemy(int x, int y) {
         super("Enemy", x, y);
     }
-
     @Override
     public void update() {
         System.out.println(name + " is attacking!");
     }
 }
+
