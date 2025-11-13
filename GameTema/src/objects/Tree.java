da8260753fa1d2a9df4a659596c6dcb8e95fd389
Ezy <146414741+SandruEzechiel@users.noreply.github.com>
2025-11-13 22:58:45 +0200

    fix

--- GameTema/src/objects/Tree.java
+++ GameTema/src/objects/Tree.java
@@ -1,14 +1,13 @@
 package objects;
-
 import game.GameObject;
 
 public class Tree extends GameObject {
     public Tree(int x, int y) {
         super("Tree", x, y);
     }
-
     @Override
     public void update() {
         System.out.println(name + " stands still.");
     }
 }
+
