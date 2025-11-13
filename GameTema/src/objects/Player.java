cc336b0e4304f07820644686d045b9abf290f5f7
Ezy <146414741+SandruEzechiel@users.noreply.github.com>
2025-11-13 22:58:06 +0200

    fix

--- GameTema/src/objects/Player.java
+++ GameTema/src/objects/Player.java
@@ -1,14 +1,13 @@
 package objects;
-
 import game.GameObject;
 
 public class Player extends GameObject {
     public Player(int x, int y) {
         super("Player", x, y);
     }
-
     @Override
     public void update() {
         System.out.println(name + " is moving...");
     }
 }
+
