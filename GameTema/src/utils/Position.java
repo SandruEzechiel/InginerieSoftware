ef86282e9925a28b29732ff7ce657cb07cd644a2
Ezy <146414741+SandruEzechiel@users.noreply.github.com>
2025-11-13 23:00:47 +0200

    fix

--- GameTema/src/utils/Position.java
+++ GameTema/src/utils/Position.java
@@ -8,17 +8,15 @@ public class Position {
         this.x = x;
         this.y = y;
     }
-
     public int getX() {
         return x;
     }
-
     public int getY() {
         return y;
     }
-
     @Override
     public String toString() {
         return "(" + x + ", " + y + ")";
     }
 }
+
